85829ad94b010a24c2573ecd00f1e5996d494624
FelipeCardona <rivenasriven@gmail.com>
2026-07-21 11:51:56 -0500

    Se agregan cambios en contacto

--- src/main/java/com/hackathon/agenda/Main.java
+++ src/main/java/com/hackathon/agenda/Main.java
@@ -1,17 +1,12 @@
 package com.hackathon.agenda;
+import com.hackathon.agenda.modelo.Contacto;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        Contacto primerContacto = new Contacto("Martin", "Parra", "311444444");
+        System.out.println(primerContacto);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
     }
 }
